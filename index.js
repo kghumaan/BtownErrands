2576071d4dd23b70b4c4ed1d893b6175fb823f03
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2016-07-03 19:05:43 -0400

    Fixing issues with JS

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCdqgGdZH8bWSMiHEM7ZoeWSNfZ04uA3Y8",
+    authDomain: "errandboi-f1cf5.firebaseapp.com",
+    databaseURL: "https://errandboi-f1cf5.firebaseio.com",
+    storageBucket: "errandboi-f1cf5.appspot.com",
+  };
+  firebase.initializeApp(config);
 // Creates a variable called databaseLink that links to our database.
   var databaseLink = new Firebase('https://errandboi-f1cf5.firebaseio.com/');  
   // Create javascript variables that link our previous HTML IDs.  Remember, we can't use regular HTML inside a script tag, so we need to use JQuery to reference any previous HTML.  A $ means we are using JQuery
@@ -7,15 +14,20 @@
   var locationField = $('#location');
   var miscField = $('#misc');
   var messageList = $('#example-messages'); // DELETE MAYBE?????
+  //alert(messageField);
   // If the enter key is pressed, push the values in the text boxes to our database.
-  messageField.keypress(function (e) {
+  function push(){
+    alert("yo");
+    messageField.keypress(function (e) {
     if (e.keyCode == 13) {                            //13 is the enter key's keycode
+      alert("yo");
       if (messageField.val() == ""){                  //Ensure that an activity was entered.
         alert("Please let us know how we can help!");
       }else{
         //push data to firebase and then clear the text box
-        databaseLink.push({name:nameField.val(), contact:contactField.val(), location:locationField.val(), misc:miscField.val(), task:messageField.val()});
+        databaseLink.push({name:nameField.val(), value:messageField.val()});
         messageField.val('');
       }
     }
+  }
   });//end of keypress function
