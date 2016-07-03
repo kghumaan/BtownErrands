b509c866e4a8d2618a3599edabf2189d8afc9efd
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2016-07-03 14:03:08 -0400

    Added a thank you page using PHP

---
+++ thanks.php
@@ -0,0 +1,36 @@
+<DOCTYPE! html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- FireBase Connection -->
+<script src="https://www.gstatic.com/firebasejs/3.1.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCdqgGdZH8bWSMiHEM7ZoeWSNfZ04uA3Y8",
+    authDomain: "errandboi-f1cf5.firebaseapp.com",
+    databaseURL: "https://errandboi-f1cf5.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="style2.css" rel="stylesheet" />
+<title>Thank you!</title>
+
+</head>
+<body>
+<div id="container">
+	<header>
+		<h1 class="title">ErrandBoi</h1>
+	</header>
+	<div id="banner">
+		<h2>Your Helping Hand in Every Situation</h2>
+	</div>
+	<div id="content">
+		<p class="content">Ever have an emergency while you are in class? Life has got you all tied up but your tasks won't do themselves? Well, you are at the right place for help. Let ErrandBoi take the stress off your shoulders while you can do what really matters. Simply, fill out the form below with any* task that you may need help with and one of our drivers will help you out as soon as possible!</p>
+</div>
+</div>
+</body>
+</html>
