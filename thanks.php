9709976a0aea4b4de78dc9854a108317c49532c2
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2016-07-03 16:30:49 -0400

    Added external JavaScript file and edited all the reference IDs to effeciently push information to DB

--- thanks.php
+++ thanks.php
@@ -26,10 +26,10 @@
 		<h1 class="title">ErrandBoi</h1>
 	</header>
 	<div id="banner">
-		<h2>Your Helping Hand in Every Situation</h2>
+		<h2>Bet, we got your back!</h2>
 	</div>
 	<div id="content">
-		<p class="content">Ever have an emergency while you are in class? Life has got you all tied up but your tasks won't do themselves? Well, you are at the right place for help. Let ErrandBoi take the stress off your shoulders while you can do what really matters. Simply, fill out the form below with any* task that you may need help with and one of our drivers will help you out as soon as possible!</p>
+		<p class="content">Someone has been informed to help you out as soon as possible! Please keep in mind that we may contact you if your instructions were unclear or if the request can not be met. We thank you for your patience!</p>
 </div>
 </div>
 </body>
