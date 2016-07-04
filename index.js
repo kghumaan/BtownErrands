50c5347a33c7e8daa928ef6f252c8932a11aad91
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2016-07-03 20:19:20 -0400

    Helping a brother out. Firebase connection now works. Fields can get added.

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-var config = {
-    apiKey: "AIzaSyCdqgGdZH8bWSMiHEM7ZoeWSNfZ04uA3Y8",
-    authDomain: "errandboi-f1cf5.firebaseapp.com",
-    databaseURL: "https://errandboi-f1cf5.firebaseio.com",
-    storageBucket: "errandboi-f1cf5.appspot.com",
-  };
-  firebase.initializeApp(config);
 // Creates a variable called databaseLink that links to our database.
   var databaseLink = new Firebase('https://errandboi-f1cf5.firebaseio.com/');  
   // Create javascript variables that link our previous HTML IDs.  Remember, we can't use regular HTML inside a script tag, so we need to use JQuery to reference any previous HTML.  A $ means we are using JQuery
-  var messageField = $('#task');
-  var nameField = $('#name');
-  var contactField = $('#contact');
-  var locationField = $('#location');
-  var miscField = $('#misc');
+  var messageField = document.getElementById('task');
+  var nameField = document.getElementById('name');
+  var contactField = document.getElementById('contact');
+  var locationField = document.getElementById('location');
+  var miscField = document.getElementById('misc');
   var messageList = $('#example-messages'); // DELETE MAYBE?????
   //alert(messageField);
   // If the enter key is pressed, push the values in the text boxes to our database.
-  function push(){
-    alert("yo");
-    messageField.keypress(function (e) {
-    if (e.keyCode == 13) {                            //13 is the enter key's keycode
-      alert("yo");
-      if (messageField.val() == ""){                  //Ensure that an activity was entered.
-        alert("Please let us know how we can help!");
-      }else{
-        //push data to firebase and then clear the text box
-        databaseLink.push({name:nameField.val(), value:messageField.val()});
-        messageField.val('');
-      }
-    }
+
+  function push() {
+    databaseLink.push({
+      name:document.getElementById('name').value, 
+      value:document.getElementById('name').value,
+      contact:document.getElementById('contact').value,
+      location:document.getElementById('location').value,
+      misc:document.getElementById('misc').value
+    });
   }
-  });//end of keypress function
