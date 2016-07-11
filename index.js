240f77cb410b2e90bc4a2bfbe96c191f141df41a
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2016-07-11 18:17:13 -0400

    Took out text capability and replaced with an email PHP form

--- index.js
+++ index.js
@@ -7,13 +7,11 @@
   var locationField = document.getElementById('location');
   var miscField = document.getElementById('misc');
   var messageList = $('#example-messages'); // DELETE MAYBE?????
-  var twilio = require('twilio');
-  var client = new twilio.RestClient('AC713965b7bfb22cfd4658cb9ff997894a', 'bfe1503835b4966d5a78e081ea5c260c');
+  // var twilio = require('twilio');
+  // var client = new twilio.RestClient('AC713965b7bfb22cfd4658cb9ff997894a', 'bfe1503835b4966d5a78e081ea5c260c');
 
   //alert(messageField);
   // If the enter key is pressed, push the values in the text boxes to our database.
-  require([twilio]), function(twilio){
-    var client = twilio(accountSid, authToken);
     function push() {
       databaseLink.push({
         name:document.getElementById('name').value, 
@@ -21,29 +19,28 @@
         contact:document.getElementById('contact').value,
         location:document.getElementById('location').value,
         misc:document.getElementById('misc').value,
-      });
+      })
     //require the Twilio module and create a REST client
     //Send an SMS text message
-    client.sms.messages.create({
+  //   client.sms.messages.create({
 
-    to:'+13177302557', // Any number Twilio can deliver to
-    from: '+13173644864', // A number you bought from Twilio and can use for outbound communication
-    body: 'Requested Task:' + messageField + nameField + contactField + locationField + miscField // body of the SMS message
-    }, function(err, responseData) { //this function is executed when a response is received from Twilio
+  //   to:'+13177302557', // Any number Twilio can deliver to
+  //   from: '+13173644864', // A number you bought from Twilio and can use for outbound communication
+  //   body: 'Requested Task:' + messageField + nameField + contactField + locationField + miscField // body of the SMS message
+  //   }, function(err, responseData) { //this function is executed when a response is received from Twilio
 
-    if (!err) { // "err" is an error received during the request, if any
+  //   if (!err) { // "err" is an error received during the request, if any
 
-        // "responseData" is a JavaScript object containing data received from Twilio.
-        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+  //       // "responseData" is a JavaScript object containing data received from Twilio.
+  //       // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+  //       // http://www.twilio.com/docs/api/rest/sending-sms#example-1
 
-        console.log(responseData.from); // outputs "+14506667788"
-        console.log(responseData.body); // outputs "word to your mother."
+  //       console.log(responseData.from); // outputs "+14506667788"
+  //       console.log(responseData.body); // outputs "word to your mother."
 
-    };
-  });
+  //   };
+  // });
   };
-};
     // databaseLink.limitToLast(100).on('child_added', function (snapshot) {
     // // databaseLink.once("name","location","misc","contact","value", function (dataSnapshot){
     //   window.location.href = "sms:3177302557?body=" + messageField + nameField + contactField + locationField + miscField;
--- thanks.php
+++ thanks.php
@@ -1,37 +1,17 @@
-<DOCTYPE! html>
-<html>
-<head>
-<meta charset="UTF-8">
 <?php
-	$CL_sms_Serv = "ErrandBoi Request:";
-	$subject = $CL_sms_Serv;
-	$to = 3177302557@sms.att.net;
-	$name = $_POST['name']
-	$contact = $_POST['contact']
-	$location = $_POST['location']
-	$misc = $_POST['misc']
-	$task = $_POST['task']
-	$message = $name and $contact and $location and $misc and $task
-
-	mail($to, $subject, $message);
+if(isset($_POST["submit"])){
+// Checking For Blank Fields..
+if($_POST["field1"]==""||$_POST["field2"]==""||$_POST["field3"]==""||$_POST["field4"]==""||$_POST["field5"]==""||$_POST["field6"]==""){
+echo "Fill All Fields..";
+}else{
+$subject = $_POST['sub'];
+$message = $_POST['field1' + ' ' + 'field2' + ' ' + 'field3' + ' ' + 'field4' + ' ' + 'field5' + ' ' + 'field6'];
+$headers = 'YOU GOT AN ERRAND: '. $email2 . "\r\n"; // Sender's Email
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = wordwrap($message, 70);
+// Send Mail By PHP Mail Function
+mail("kghumaan@umail.iu.edu", $subject, $message, $headers);
+echo "Your request has been sent successfuly! Thank you for your feedback";
+}
+}
 ?>
-
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link href="style2.css" rel="stylesheet" />
-<title>Thank you!</title>
-
-</head>
-<body>
-<div id="container">
-	<header>
-		<h1 class="title">ErrandBoi</h1>
-	</header>
-	<div id="banner">
-		<h2>Bet, we got your back!</h2>
-	</div>
-	<div id="content">
-		<p class="content">Someone has been informed to help you out as soon as possible! Please keep in mind that we may contact you if your instructions were unclear or if the request can not be met. We thank you for your patience!</p>
-</div>
-</div>
-</body>
-</html>
