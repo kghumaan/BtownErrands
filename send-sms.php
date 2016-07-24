dc871167db8baac2c9b22fb33efab4fe5e2c6133
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2016-07-23 23:19:56 -0400

    Fixed PHP push to accomodate for new input fields.

--- send-sms.php
+++ send-sms.php
@@ -4,9 +4,10 @@ require 'twilio-php-master/Services/Twilio.php';
 
 $name = $_POST['field1'];
 $task = $_POST['field6'];
-$contact = $_POST['field2'];
+$email = $_POST['field2'];
 $location = $_POST['field3'];
 $misc = $_POST['field4'];
+$phone = $_POST['field5'];
 
 $account_sid = 'AC713965b7bfb22cfd4658cb9ff997894a'; 
 $auth_token = 'bfe1503835b4966d5a78e081ea5c260c'; 
@@ -15,7 +16,7 @@ $client = new Services_Twilio($account_sid, $auth_token);
 $client->account->messages->create(array( 
 	'To' => "3177302557", 
 	'From' => "+13173644864", 
-	'Body' => "YOU GOT A NEW ERRAND, BET!: \nName: $name \nTask: $task \nContact: $contact \nLocation: $location \nMisc: $misc",   
+	'Body' => "YOU GOT A NEW ERRAND, BET!: \nName: $name \nTask: $task \nEmail: $email \nPhone #: $phone \nLocation: $location \nMisc: $misc",   
 )); 
 
 ?>
